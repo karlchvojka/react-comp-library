e72c452c21b5f81879f97f6a72fd8be5c56a5f10
karl.chvojka <karl.chvojka@protonmail.com>
2020-05-07 18:15:30 -0400

    Initial Setup

---
+++ example/src/Components/Templates/component_wiki/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const Comp_Layout = () => {
+  return (
+    <div>
+      <h1>Welcome to the Gli7ched Comp Library</h1>
+    </div>
+  )
+}
+
+export default Comp_Layout;
