d7be5e6e0db552ab179988be260c7b03c5223a4e
karl.chvojka <karl.chvojka@protonmail.com>
2020-05-07 18:07:00 -0400

    Work on Layout and button

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,43 @@
+// This Apps Imports
 import React from 'react'
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import './App.scss'
+import Home from './Components/Pages/index.js';
+
+// Component Example Imports
 import { Button } from 'blackburn-comp-library';
 import 'blackburn-comp-library/dist/index.scss';
 
 const App = () => {
   return (
-  <>
+  <BrowserRouter>
     <header>
-      <p>This is the header</p>
+      <h1>Gli7ched's Component Library</h1>
     </header>
     <main>
       <aside className="mainSidebar">
+        <h2>Component Navigation</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/button">Button</Link>
+            </li>
+          </ul>
+        </nav>
       </aside>
-      <section className="mainBody">
-        <Button text="Gli7ches Stream is THA BEST" />
-      </section>
+      <Switch className="mainBody">
+        <Route path="/button">
+          <Button text="HI Chat" />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </main>
-  </>
+  </BrowserRouter>
   )
 }
 
