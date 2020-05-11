491fc9393d36620639b2319315f3886447de72ec
karl.chvojka <karl.chvojka@protonmail.com>
2020-05-11 13:52:18 -0400

    Merge issue

--- src/Components/Atoms/Button/Button.js
+++ src/Components/Atoms/Button/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Button({ text }) {
-  return <button>{text}</button>
+export default function Button({ text, name, type, value, className }) {
+  return <button className={className} name={name} type={type} value={value}>{text}</button>
 }
