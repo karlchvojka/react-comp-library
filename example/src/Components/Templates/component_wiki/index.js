9226cc6dbabf9204bb6191401826fef38bf017b2
karl.chvojka <karl.chvojka@protonmail.com>
2020-05-10 20:21:03 -0400

    Cleaned up some basic variable stuff

--- example/src/Components/Templates/component_wiki/index.js
+++ example/src/Components/Templates/component_wiki/index.js
@@ -13,25 +13,25 @@ const CompLayout = (props) => {
       {/* Title will be individual for each component. */}
       <h1>This page for: A Component</h1>
 
-      <h2>Description</h2>
+      <h2>Description:</h2>
       {/* Add Description of the usage of the component and other information here */}
       <p>Its a Button.. Duh..</p>
 
-      <h2>Example</h2>
+      <h2>Example:</h2>
       {/* Add component here. */}
 
-      <h2>Props</h2>
+      <h2>Props:</h2>
       <p>See documentation of the usage of the props for this component below.</p>
       <section className="propsTable">
         {/* Props Table Headers */}
         <div className="colTitle">
-          <p>Name</p>
+          <p>Name:</p>
         </div>
         <div className="colTitle">
-          <p>Description</p>
+          <p>Description:</p>
         </div>
         <div className="colTitle">
-          <p>Example</p>
+          <p>Example:</p>
         </div>
 
         {/* Prop Example */}
